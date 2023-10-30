998d09324f3fc0f00f4c393cb741570a1b711508
yuanoung <yuanoung@qq.com>
2023-10-30 19:59:58 +0800

    bs_test modify

--- binarySearch/bs_test.go
+++ binarySearch/bs_test.go
@@ -3,34 +3,36 @@ package binarySearch
 import "testing"
 
 func TestBs0704(t *testing.T) {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+
 	// test 1
-	if got := Bs0704([]int{-1, 0, 3, 5, 9, 12}, -2); got != -1 {
+	if got := Bs0704(nums, -2); got != -1 {
 		t.Errorf("test 1, got: %d, want: -1", got)
 	}
 
 	// test 2
-	if got := Bs0704([]int{-1, 0, 3, 5, 9, 12}, -1); got != 0 {
+	if got := Bs0704(nums, -1); got != 0 {
 		t.Errorf("test 2, got: %d, want: 0", got)
 	}
 
 	// test 3
-	if got := Bs0704([]int{-1, 0, 3, 5, 9, 12}, 3); got != 2 {
+	if got := Bs0704(nums, 3); got != 2 {
 		t.Errorf("test 3, got: %d, want: 2", got)
 	}
 
 	// test 4
-	if got := Bs0704([]int{-1, 0, 3, 5, 9, 10, 12, 15}, 12); got != 6 {
-		t.Errorf("test 4, got: %d, want: 0", got)
+	if got := Bs0704(nums, 12); got != 5 {
+		t.Errorf("test 4, got: %d, want: 6", got)
 	}
 
 	// test 5
-	if got := Bs0704([]int{-1, 0, 3, 5, 9, 10, 12, 15}, 15); got != 7 {
-		t.Errorf("test 5, got: %d, want: 0", got)
+	if got := Bs0704(nums, 15); got != -1 {
+		t.Errorf("test 5, got: %d, want: -1", got)
 	}
 
-	// test 6
-	if got := Bs0704([]int{-1, 0, 3, 5, 9, 10, 12, 15}, 30); got != -1 {
-		t.Errorf("test 6, got: %d, want: 0", got)
+	// test 7
+	if got := Bs0704([]int{}, 2); got != -1 {
+		t.Errorf("test 6, got: %d, want: -1", got)
 	}
 }
 
